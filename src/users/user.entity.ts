df7c908dc46a17217c94ce2b8a8e2b121a5463b6
sahanaydn <sahanaydn1@hotmail.com>
2024-05-21 01:15:10 -0500

    plain data vs entity (hooks)

--- src/users/user.entity.ts
+++ src/users/user.entity.ts
@@ -1,10 +1,31 @@
-import { Entity, Column, PrimaryGeneratedColumn } from 'typeorm';
+import {
+  AfterInsert,
+  AfterRemove,
+  AfterUpdate,
+  Entity,
+  Column,
+  PrimaryGeneratedColumn,
+} from 'typeorm';
 @Entity()
 export class User {
   @PrimaryGeneratedColumn()
   id: number;
   @Column()
   email: string;
+
   @Column()
   password: string;
+  @AfterInsert()
+  logInsert() {
+    console.log('Inserted user with id ', this.id);
+  }
+  @AfterUpdate()
+  logUpdate() {
+    console.log('Updated user with id ', this.id);
+  }
+
+  @AfterRemove()
+  logRemove() {
+    console.log('Removed user with id ', this.id);
+  }
 }
--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,4 +1,18 @@
 import { Injectable } from '@nestjs/common';
+import { Repository } from 'typeorm';
+import { InjectRepository } from '@nestjs/typeorm';
+import { User } from './user.entity';
 
 @Injectable()
-export class UsersService {}
+export class UsersService {
+  constructor(@InjectRepository(User) private repo: Repository<User>) {}
+
+  create(email: string, password: string) {
+    //turn into an instance of a user entity and then make use of the users repository to actually save this new user our database
+    //dont forget respository is created for us automatically by TYPE ORM.
+    //create komutu database'e birsey kaydetmez onun yerine elimizdeki verilerle bir entity olusturur ve bu entity repository tarafindan kullanilir.
+    const user = this.repo.create({ email, password });
+    //repository'e entity parametresi verirsek user entitydeki hooklar calisacak ancak plain object verirsek user entitydeki hookslar calismayacak
+    return this.repo.save({ email, password });
+  }
+}
