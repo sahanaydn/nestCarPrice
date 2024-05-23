f05a0be8f0e74088e63aeb15b90cbe03058695d0
sahanaydn <sahanaydn1@hotmail.com>
2024-05-23 02:15:37 -0500

    created some routes

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,4 +1,13 @@
-import { Controller, Post, Body } from '@nestjs/common';
+import {
+  Controller,
+  Post,
+  Body,
+  Patch,
+  Get,
+  Param,
+  Query,
+  Delete,
+} from '@nestjs/common';
 import { CreateUserDto } from './dtos/create-user-dto';
 import { UsersService } from './users.service';
 
@@ -9,5 +18,22 @@ export class UsersController {
   createUser(@Body() body: CreateUserDto) {
     console.log(body);
     this.usersService.create(body.email, body.password);
+  }
+  @Get('/:id')
+  findUser(@Param('id') id: string) {
+    //The id in the URL comes as a string, but since the id in the service is a type number We made a type conversion
+    return this.usersService.findOne(parseInt(id));
+  }
+
+  @Get()
+  findAllUsers(@Query('email') email: string) {
+    // example Get request  http://localhost:3000/auth?email=temp@email.com
+    return this.usersService.find(email);
+  }
+  @Delete('/:id')
+  removeUser(@Param('id') id: string) {
+    //The id in the URL comes as a string, but since the id in the service is a type number We made a type conversion
+
+    return this.usersService.remove(parseInt(id));
   }
 }
